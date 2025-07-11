0b86cc792991abfd8378a12b4860c9bf20f78b1b
TokalaVaishnavi <vaishnavitokala002@gmail.com>
2025-07-11 15:52:05 +0530

    Added week-4 handson
    Adding week-4 handson

---
+++ 2.spring-rest-handson/Hello World RESTful Web Service/code/src/main/java/a/hellorestservice/HelloRestServiceApplication.java
@@ -0,0 +1,12 @@
+package a.hellorestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRestServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloRestServiceApplication.class, args);
+	}
+}
+
---
+++ JWT handson/Create authentication service that returns JWT/JwtAuthApp/code/src/test/java/com/cognizant/JwtAuthApp/JwtAuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.JwtAuthApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
