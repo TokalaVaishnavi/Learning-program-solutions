55fdb1c3996dd3320efcb6a2c9ad554ee789730f
TokalaVaishnavi <vaishnavitokala002@gmail.com>
2025-07-11 16:33:06 +0530

    ADDED WEEK 4 HANDS-ON

---
+++ week-4/JWT handson/Create authentication service that returns JWT/JwtAuthApp/code/src/main/java/com/cognizant/JwtAuthApp/JwtAuthAppApplication.java
@@ -0,0 +1,14 @@
+package com.cognizant.JwtAuthApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthAppApplication.class, args);
+	}
+}
+
+
+
