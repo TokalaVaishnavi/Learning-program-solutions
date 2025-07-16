c92dfd57c6136988f209ab7075c6214f5c694d16
TokalaVaishnavi <vaishnavitokala002@gmail.com>
2025-07-16 21:38:31 +0530

    Added Week-5 handson
    adding week-5 handson

---
+++ week-5/employee id/microservices/loan/code/src/main/java/com/cognizant/loan/controller/LoanController.java
@@ -0,0 +1,27 @@
+package com.cognizant.loan.controller;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+
+    @GetMapping("/{number}")
+    public Map<String, Object> getLoanDetails(@PathVariable String number) {
+        return Map.of(
+                "number", number,
+                "type", "car",
+                "loan", 400000,
+                "emi", 3258,
+                "tenure", 18
+        );
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "Loan service is running!";
+    }
+}
+
+
